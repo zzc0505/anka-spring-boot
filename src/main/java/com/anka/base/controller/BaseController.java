0274b548e271386a67d5c4d9b9ef11bf94522b90
zzc0505 <zzc0505@qq.com>
2019-02-20 21:57:16 +0800

    优化

--- src/main/java/com/anka/base/controller/BaseController.java
+++ src/main/java/com/anka/base/controller/BaseController.java
@@ -18,25 +18,22 @@ public class BaseController<T extends BaseModel<T>> {
 	}
 
 	/**
-	 * 通用页面跳转
-	 * 
+	 * @Description: 通用页面跳转
+	 * @author AnkaRebirth
 	 * @param view
 	 * @return
 	 */
 	@RequestMapping("/{view}")
-	public String view(@PathVariable String view) {
+	public String getView(@PathVariable String view) {
 		String path = "";
 		try {
 			T model = modelClass.newInstance();
-			// 得到字节码文件 【只需要更改controller类名】
 			try {
 				Class<?> clazz = Class
 						.forName(model.getClass().getName().replace("model", "controller") + "Controller");
 				boolean present = clazz.isAnnotationPresent(RequestMapping.class);
 				if (present) {
-					// 得到requestMapping注释
 					RequestMapping annotation = clazz.getAnnotation(RequestMapping.class);
-					// 得到value数组
 					String[] value = annotation.value();
 					path = value[0] + "/";
 				}
