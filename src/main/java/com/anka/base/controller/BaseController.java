cba4fef8392e815b2cf27e2d761c8f57e9cc2fa7
zzc0505 <zzc0505@qq.com>
2019-02-20 20:45:00 +0800

    controller基层重新编写转发逻辑

--- src/main/java/com/anka/base/controller/BaseController.java
+++ src/main/java/com/anka/base/controller/BaseController.java
@@ -16,27 +16,38 @@ public class BaseController<T extends BaseModel<T>> {
 		ParameterizedType pt = (ParameterizedType) this.getClass().getGenericSuperclass();
     	modelClass = (Class<T>) pt.getActualTypeArguments()[0];
 	}
+
 	/**
 	 * 通用页面跳转
+	 * 
 	 * @param view
 	 * @return
 	 */
 	@RequestMapping("/{view}")
-	public String view(@PathVariable String view){
+	public String view(@PathVariable String view) {
 		String path = "";
 		try {
 			T model = modelClass.newInstance();
-			String modelName = model.getClass().getName().substring(
-					model.getClass().getName().lastIndexOf(".")+1, model.getClass().getName().length());
-			String[] temp = modelName.replaceAll("[A-Z]", " $0").trim().split(" ");
-			for (String t : temp) {
-				path+=t.toLowerCase()+"/";
+			// 得到字节码文件 【只需要更改controller类名】
+			try {
+				Class<?> clazz = Class
+						.forName(model.getClass().getName().replace("model", "controller") + "Controller");
+				boolean present = clazz.isAnnotationPresent(RequestMapping.class);
+				if (present) {
+					// 得到requestMapping注释
+					RequestMapping annotation = clazz.getAnnotation(RequestMapping.class);
+					// 得到value数组
+					String[] value = annotation.value();
+					path = value[0] + "/";
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
 		} catch (InstantiationException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		}
-		return path+view;
+		return path + view;
 	}
 }
