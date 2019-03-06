c0451c6418fb5117fc1e083d5fd1393c18bec39b
zzc0505 <zzc0505@qq.com>
2019-03-06 21:23:30 +0800

    修复根据uuid批量删除、批量查询BUG

--- src/main/java/com/anka/base/service/CrudBaseServiceSupport.java
+++ src/main/java/com/anka/base/service/CrudBaseServiceSupport.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 
 import com.anka.base.excepiton.BusinessException;
 import com.anka.base.mapper.CrudBaseMapper;
@@ -64,8 +63,15 @@ public abstract class CrudBaseServiceSupport<T extends BaseModel<T>> implements 
 			baseCode.setMsg("批量删除时，model属性strList不能为空！");
 			throw new BusinessException(baseCode);
 		}
-		String ids = StringUtils.collectionToCommaDelimitedString(model.getStrList());
-		return mapper.deleteByIds(ids);
+		StringBuffer sb = new StringBuffer();
+		for (int i=0;i<model.getStrList().size();i++) {
+			if(i==model.getStrList().size()-1){
+				sb.append("'" + model.getStrList().get(i) + "'");
+			}else{
+				sb.append("'" + model.getStrList().get(i) + "',");
+			}
+		}
+		return mapper.deleteByIds(sb.toString());
 	}
 
 	@Override
@@ -115,8 +121,15 @@ public abstract class CrudBaseServiceSupport<T extends BaseModel<T>> implements 
 			baseCode.setMsg("更具ids批量查询时，model属性strList不能为空！");
 			throw new BusinessException(baseCode);
 		}
-		String ids = StringUtils.collectionToCommaDelimitedString(model.getStrList());
-		return mapper.selectByIds(ids);
+		StringBuffer sb = new StringBuffer();
+		for (int i=0;i<model.getStrList().size();i++) {
+			if(i==model.getStrList().size()-1){
+				sb.append("'" + model.getStrList().get(i) + "'");
+			}else{
+				sb.append("'" + model.getStrList().get(i) + "',");
+			}
+		}
+		return mapper.selectByIds(sb.toString());
 	}
 
 	@Override
